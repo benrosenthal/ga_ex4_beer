87103fcaaca1bffd4ce94636d5f204422fe17492
Kevin Galligan <kgalligan@gmail.com>
2015-12-08 13:39:07 -0500

    Completed

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -1,5 +1,7 @@
 package org.gradle;
 
+import java.util.Scanner;
+
 /**
  * Created by kgalligan on 12/2/15.
  */
@@ -12,12 +14,23 @@ public class App
 		// if age is less than 21, display "Nay, don't have a beer!"
 
 		// ask for the user's age
+		System.out.print("What is your age? ");
 
 		// store the user input in a variable: user_age
+		Scanner inp = new Scanner(System.in);
+		int userAge = Integer.parseInt(inp.nextLine());
 
 		// run the function
+		canHazBeer(userAge);
 	}
 
+	static void canHazBeer(int age)
+	{
+		if(age >= 21)
+			System.out.println("Yay, have a beer!");
+		else
+			System.out.println("Nay, don't have a beer!");
+	}
 }
 
 
